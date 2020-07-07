b601b929bf440597039be5ee67b31658a7b1d5b3
srinskit <srinskit@users.noreply.github.com>
2020-07-07 10:31:27 +0530

    Added history and volumes

--- src/main/java/io/srinskit/apiserver/APIServerVerticle.java
+++ src/main/java/io/srinskit/apiserver/APIServerVerticle.java
@@ -4,8 +4,10 @@ import io.srinskit.adder.AdderService;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.core.http.HttpServerResponse;
+import java.io.*;
 
 public class APIServerVerticle extends AbstractVerticle {
+	static String historyFileName = "/data/api-server-history.txt";
 
 	@Override
 	public void start() {
@@ -18,12 +20,41 @@ public class APIServerVerticle extends AbstractVerticle {
 			Integer x = new Integer(routingContext.request().getParam("x"));
 			Integer y = new Integer(routingContext.request().getParam("y"));
 			adderService.operate(x, y, res -> {
+				String reply = "";
 				if (res.succeeded()) {
-					response.end(res.result().toString());
+					reply = String.format("%d + %d = %d\n", x, y, res.result());
 				} else {
-					response.end("ERROR" + res.cause());
+					reply = String.format("%d + %d = %s\n", x, y, "ERROR, " + res.cause());
 				}
+				try {
+					FileWriter fileWriter = new FileWriter(historyFileName, true);
+					fileWriter.write(reply);
+					fileWriter.close();
+				} catch (IOException ex) {
+					System.out.println("Error writing to history file");
+					reply = "ERROR: " + ex.getMessage();
+				}
+				response.end(reply);
 			});
+		});
+
+		router.route("/history").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response.sendFile(historyFileName);
+			response.end();
+		});
+
+		router.route("/clear_history").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			String reply = "Done";
+			try {
+				FileWriter fileWriter = new FileWriter(historyFileName);
+				fileWriter.close();
+			} catch (IOException ex) {
+				System.out.println("Error writing to history file");
+				reply = "ERROR: " + ex.getMessage();
+			}
+			response.end(reply);
 		});
 
 		vertx.createHttpServer().requestHandler(router).listen(8080);
